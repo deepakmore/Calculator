d60a8bbd2e914ddb0768dd2c497992723d04ccba
Deepak More <deepakmore@outlook.com>
2021-11-09 11:26:00 +0530

    Carent - exponents added

--- src/main/java/com/tech/app/constants/CalculatorConstants.java
+++ src/main/java/com/tech/app/constants/CalculatorConstants.java
@@ -1,7 +1,7 @@
 package com.tech.app.constants;
 
 public class CalculatorConstants {
-    public static final String SUPPORTED_OPERATORS = "+-%*";
-    public static final String ONLY_DIGITS_AND_MATH_OPERATORS_REGEX = "^[0-9()+\\-*.\\/]*$";
+    public static final String SUPPORTED_OPERATORS = "+-%*^";
+    public static final String ONLY_DIGITS_AND_MATH_OPERATORS_REGEX = "^[0-9()+\\-*.\\/^]*$";
     public static final String USER_INPUT_MESSAGE = "Please Enter Expression - ";
 }
