4b44176477dd0c47c88633038a224199628903e5
Deepak More <deepakmore@outlook.com>
2021-11-10 18:54:54 +0530

    Parenthesis support not added yet

--- src/main/java/com/tech/app/exceptions/InvalidExpressionException.java
+++ src/main/java/com/tech/app/exceptions/InvalidExpressionException.java
@@ -2,6 +2,6 @@ package com.tech.app.exceptions;
 
 public class InvalidExpressionException extends RuntimeException {
     public InvalidExpressionException() {
-        super("Invalid Expression");
+        super("Parenthesis support not added - please remove it or Invalid Expression");
     }
 }
