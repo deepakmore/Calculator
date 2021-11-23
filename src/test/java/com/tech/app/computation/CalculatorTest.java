dc37c3da3e02370509072df0cdb5d7d2ebedaede
Deepak More <deepakmore@outlook.com>
2021-11-23 14:59:51 +0530

    Change unit test case

--- src/test/java/com/tech/app/computation/CalculatorTest.java
+++ src/test/java/com/tech/app/computation/CalculatorTest.java
@@ -3,25 +3,18 @@ package com.tech.app.computation;
 import com.tech.app.enums.OperatorsEnum;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
-    @Mock
-    Calculator calculator;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
+    Calculator calculator = Calculator.getInstance();
 
     @Test
     public void shouldGetOperatorPrecedence() {
         String expression = "^+-/*";
-        calculator = new Calculator(expression);
+        calculator.setExpression(expression);
 
         int precedence = calculator.getOperatorPrecedence(OperatorsEnum.CARET);
         assertEquals("Caret operator does not match", 3, precedence);
@@ -41,29 +34,32 @@ public class CalculatorTest {
 
     @Test
     public void shouldGetResultPerOperation() {
-        String expression = "^+-/*";
-        calculator = new Calculator(expression);
+        String expression = "^+-/*#";
+        calculator.setExpression(expression);
 
-        double actualValue = calculator.applyOperator(10, OperatorsEnum.ADD, 2);
+        double actualValue = calculator.applyOperator(OperatorsEnum.ADD,10 , 2);
         assertEquals("ADd operator does not match", 12, actualValue, 0.0);
 
-        actualValue = calculator.applyOperator(10, OperatorsEnum.SUBTRACT, 2);
+        actualValue = calculator.applyOperator(OperatorsEnum.SUBTRACT,10, 2);
         assertEquals("Subtract operator does not match", 8, actualValue, 0.0);
 
-        actualValue = calculator.applyOperator(10, OperatorsEnum.DIVIDE, 2);
+        actualValue = calculator.applyOperator(OperatorsEnum.DIVIDE,10, 2);
         assertEquals("Divide operator does not match", 5, actualValue, 0.0);
 
-        actualValue = calculator.applyOperator(10, OperatorsEnum.MULTIPLY, 2);
+        actualValue = calculator.applyOperator(OperatorsEnum.MULTIPLY,10,  2);
         assertEquals("Multiply operator does not match", 20, actualValue, 0.0);
 
-        actualValue = calculator.applyOperator(10, OperatorsEnum.CARET, 2);
+        actualValue = calculator.applyOperator(OperatorsEnum.CARET,10,  2);
         assertEquals("Caret operator does not match", 100, actualValue, 0.0);
+
+        actualValue = calculator.applyOperator(OperatorsEnum.HASH,  2);
+        assertEquals("Caret operator does not match", 3.0, actualValue, 0.0);
     }
 
     @Test
     public void shouldPreprocessExpression() {
         String expression = "10+20";
-        calculator = new Calculator(expression);
+        calculator.setExpression(expression);
 
         calculator.preprocess();
         assertEquals("Expression does not change", "10.0+20.0", calculator.getExpression());
@@ -72,7 +68,7 @@ public class CalculatorTest {
     @Test
     public void shouldEvaluateExpression() {
         String expression = "10+20";
-        calculator = new Calculator(expression);
+        calculator.setExpression(expression);
 
         double actualValue = calculator.evaluateExpression();
 
@@ -82,7 +78,7 @@ public class CalculatorTest {
     @Test
     public void shouldEvaluateExpressionTwo() {
         String expression = "10+20*50/5-150";
-        calculator = new Calculator(expression);
+        calculator.setExpression(expression);
 
         double actualValue = calculator.evaluateExpression();
 
