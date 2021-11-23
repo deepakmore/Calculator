57aa7c57ddec44acdfdd7b1f533533aff33da5bd
Deepak More <deepakmore@outlook.com>
2021-11-23 15:03:53 +0530

    Added Hash symbol in application

--- src/main/java/com/tech/app/constants/CalculatorConstants.java
+++ src/main/java/com/tech/app/constants/CalculatorConstants.java
@@ -3,7 +3,7 @@ package com.tech.app.constants;
 public class CalculatorConstants {
     public static final String SUPPORTED_OPERATORS = "+-%*^";
     public static final String FIRST_PLACE_SUPPORTED_OPERATORS = "+%*^";
-    public static final String ONLY_DIGITS_AND_MATH_OPERATORS_REGEX = "^[0-9()+\\-*.\\/^]*$";
+    public static final String ONLY_DIGITS_AND_MATH_OPERATORS_REGEX = "^[0-9()+\\-*.#\\/^]*$";
     public static final String USER_INPUT_MESSAGE = "Please Enter Expression - ";
     public static final String ONLY_DIGITS_WITH_DECIMAL_POINTER_REGEX = "^[0-9.()]*$";
     public static final String ONLY_OPERATORS_WITH_DECIMAL_POINTER_REGEX = "^[+\\-\\/+.()^]*$";
